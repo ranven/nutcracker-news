c6fe0042f6e0a40404d811d486a4c68f6872e4a5
ranven <venla.e.rantanen@gmail.com>
2023-02-17 15:49:00 +0200

    add comment count for all posts

--- posts.py
+++ posts.py
@@ -3,7 +3,8 @@ from db import db
 def get_all_posts(user_id):
     if user_id == 0:
         sql = """SELECT p.title, p.content, p.created_at, p.post_id, u.user_id, u.username, 
-        (SELECT COALESCE(SUM((v.vote_code)::int)-SUM((not v.vote_code)::int), 0) as votes FROM votes v WHERE v.post_id = p.post_id) 
+        (SELECT COALESCE(SUM((v.vote_code)::int)-SUM((not v.vote_code)::int), 0) as votes FROM votes v WHERE v.post_id = p.post_id),
+        (SELECT COUNT(*) as comment_count FROM comments c WHERE c.post_id = p.post_id)
         FROM posts p
         LEFT JOIN users u ON p.user_id = u.user_id
         LEFT JOIN votes v ON v.post_id = p.post_id
@@ -12,7 +13,8 @@ def get_all_posts(user_id):
     else:
         sql = """SELECT p.title, p.content, p.created_at, p.post_id, u.user_id, u.username,
         (SELECT vc.vote_code FROM votes vc WHERE vc.post_id = p.post_id and vc.user_id = (:user_id)) as has_voted, 
-        (SELECT COALESCE(SUM((v.vote_code)::int)-SUM((not v.vote_code)::int), 0) as votes FROM votes v WHERE v.post_id = p.post_id) 
+        (SELECT COALESCE(SUM((v.vote_code)::int)-SUM((not v.vote_code)::int), 0) as votes FROM votes v WHERE v.post_id = p.post_id),
+        (SELECT COUNT(*) as comment_count FROM comments c WHERE c.post_id = p.post_id)
         FROM posts p
         LEFT JOIN users u ON p.user_id = u.user_id
         LEFT JOIN votes v ON v.post_id = p.post_id AND v.user_id != (:user_id)
