cce285cdde6830cef2c3b4d8d1cae5cf5a09a623
ranven <venla.e.rantanen@gmail.com>
2023-03-16 13:46:01 +0200

    formatting: run prettier on templates&styles and tslint on imports

--- services/routes.py
+++ services/routes.py
@@ -1,23 +1,28 @@
 from app import app
 from flask import flash
 from flask import render_template, request, redirect
-import services.auth as auth, services.posts as posts, services.profiles as profiles, services.votes as votes, services.comments as comments
+import services.auth as auth
+import services.posts as posts
+import services.profiles as profiles
+import services.votes as votes
+import services.comments as comments
 import services.helpers as helpers
+
 
 @app.route("/")
 def index():
     return redirect("/posts")
 
-#auth
+# auth
 @app.route("/login", methods=["POST", "GET"])
 def login():
     if request.method == "GET":
         return render_template("login.html")
-    
+
     if request.method == "POST":
         username = request.form["username"]
         password = request.form["password"]
-        
+
         if helpers.validate_length(username, "username") and auth.login(username, password):
             flash("Login successful!")
             return redirect("/posts")
@@ -25,11 +30,12 @@ def login():
             flash("Invalid password or username.")
             return redirect("/login")
 
+
 @app.route("/signup", methods=["POST", "GET"])
 def signup():
     if request.method == "GET":
         return render_template("signup.html")
-    
+
     if request.method == "POST":
         username = request.form["username"]
         password = request.form["password"]
@@ -37,21 +43,23 @@ def signup():
             if auth.signup(username, password):
                 profiles.create_profile()
                 flash("Successfully signed up!")
-                return redirect("/posts") 
+                return redirect("/posts")
             else:
                 flash("This username is taken. Please try again")
                 return redirect("/signup")
         else:
             flash("Username or password doesn't match requirements.")
             return redirect("/signup")
-        
-    
+
+
 @app.route("/logout")
 def logout():
     auth.logout()
     return redirect("/")
 
-#posts
+# posts
+
+
 @app.route("/posts", methods=["POST", "GET"])
 def post():
     authenticated_user = auth.user_id()
@@ -59,12 +67,13 @@ def post():
         if request.args.get("sort_by"):
             sort_param = request.args.get("sort_by")
             search_term = request.args.get("search_term")
-        else: 
+        else:
             sort_param = "new"
             search_term = ""
-        all_posts = posts.get_all_posts(authenticated_user, sort_param, search_term)
+        all_posts = posts.get_all_posts(
+            authenticated_user, sort_param, search_term)
         return render_template("posts.html", posts=all_posts, sort_by=sort_param)
-    
+
     if request.method == "POST":
         title = request.form["title"]
         content = request.form["content"]
@@ -77,7 +86,8 @@ def post():
                 return redirect("login")
         else:
             flash("Maximum length of title or content exceeded.")
-        
+
+
 @app.route("/posts/<post_id>", methods=["GET"])
 def get_post(post_id):
     authenticated_user = auth.user_id()
@@ -85,12 +95,15 @@ def get_post(post_id):
     post_comments = comments.get_comments(post_id)
     return render_template("post.html", post=post, post_comments=post_comments)
 
+
 @app.route("/posts/create", methods=["GET"])
 def create_post():
     if request.method == "GET":
         return render_template("create.html")
-        
-#comments
+
+# comments
+
+
 @app.route("/posts/<post_id>/comments", methods=["POST"])
 def send_comment(post_id):
     authenticated_user = auth.user_id()
@@ -105,7 +118,9 @@ def send_comment(post_id):
     else:
         flash("Maximum length of content exceeded.")
 
-#profiles     
+# profiles
+
+
 @app.route("/users/<user_id>", methods=["POST", "GET"])
 def profile(user_id):
     is_admin = False
@@ -113,13 +128,13 @@ def profile(user_id):
 
     if int(user_id) == authenticated_user:
         is_admin = True
-        
+
     if request.method == "GET":
         profile = profiles.get_profile(user_id)
         users_posts = posts.get_users_posts(user_id)
         users_comments = comments.get_users_comments(user_id)
         return render_template("profile.html", profile=profile, posts=users_posts, comments=users_comments, admin=is_admin)
-    
+
     if request.method == "POST":
         if is_admin:
             description = request.form["description"]
@@ -134,19 +149,23 @@ def profile(user_id):
             else:
                 flash("Maximum length of content exceeded.")
 
-#votes
+# votes
+
+
 @app.route("/likes", methods=["POST"])
 def send_vote():
     authenticated_user = auth.user_id()
     post_id = request.form["post_id"]
     vote_code = True if request.form["vote_button"] == "+1" else False
     if votes.send_vote(authenticated_user, post_id, vote_code):
-        return redirect('/posts/'+post_id) 
+        return redirect('/posts/'+post_id)
     else:
         flash("Error voting this post.")
         return redirect('/posts')
 
-#edit/delete posts/comments
+# edit/delete posts/comments
+
+
 @app.route("/edit/<content_type>/<content_id>", methods=["GET", "POST"])
 def modify_content(content_id, content_type):
     if request.method == "GET":
@@ -156,7 +175,7 @@ def modify_content(content_id, content_type):
         elif content_type == "comment":
             comment = comments.get_comment(content_id)
             return render_template("edit.html", comment=comment, post=None)
-            
+
     elif request.method == "POST":
         authenticated_user = auth.user_id()
         method = request.form["method"]
@@ -170,12 +189,12 @@ def modify_content(content_id, content_type):
                         return redirect("/edit/post/"+content_id)
                 else:
                     flash("Maximum length of title or content exceeded.")
-                
+
             case "post", "delete":
                 if posts.delete_post(content_id, authenticated_user):
                     flash("Post deleted!")
                     return redirect("/users/"+str(authenticated_user))
-                
+
             case "comment", "put":
                 content = request.form["content"]
                 if helpers.validate_length(content, "comment_content"):
@@ -184,17 +203,20 @@ def modify_content(content_id, content_type):
                         return redirect("/edit/comment/"+content_id)
                 else:
                     flash("Maximum length of title or content exceeded.")
-                    
+
             case "comment", "delete":
                 if comments.delete_comment(content_id, authenticated_user):
                     flash("Comment deleted!")
                     return redirect("/users/"+str(authenticated_user))
 
-#filter for timestamps
+# filter for timestamps
+
+
 @app.template_filter('datetimeformat')
 def datetime_format(value, format="%-d %b / %H:%M"):
     return value.strftime(format)
 
+
 @app.errorhandler(404)
-def not_found(e):
-    return render_template('error.html', code=404 ,err="This page does not exist :(")
+def not_found():
+    return render_template('error.html', code=404, err="This page does not exist :(")
