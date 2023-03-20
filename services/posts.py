f1096cfe00fc6562dc4ac635b823a33a3d82151a
ranven <venla.e.rantanen@gmail.com>
2023-03-20 13:40:16 +0200

    fix in post listing: display latest change as the post timestamp

--- services/posts.py
+++ services/posts.py
@@ -2,7 +2,7 @@ from services.db import db
 
 def get_all_posts(user_id, sort_param, search_term):
     if user_id == 0:
-        sql = """SELECT p.title, p.content, p.created_at, p.post_id, u.user_id, u.username, 
+        sql = """SELECT p.title, p.content, p.created_at, p.edited_at, p.post_id, u.user_id, u.username, 
         (SELECT COALESCE(SUM((v.vote_code)::int)-SUM((not v.vote_code)::int), 0) as votes FROM votes v WHERE v.post_id = p.post_id),
         (SELECT COUNT(*) as comment_count FROM comments c WHERE c.post_id = p.post_id)
         FROM posts p
@@ -12,7 +12,7 @@ def get_all_posts(user_id, sort_param, search_term):
         GROUP BY(p.post_id, u.user_id, p.created_at)
         """
     else:
-        sql = """SELECT p.title, p.content, p.created_at, p.post_id, u.user_id, u.username,
+        sql = """SELECT p.title, p.content, p.created_at, p.edited_at, p.post_id, u.user_id, u.username,
         (SELECT vc.vote_code FROM votes vc WHERE vc.post_id = p.post_id and vc.user_id = (:user_id)) as vote_code, 
         (SELECT COALESCE(SUM((v.vote_code)::int)-SUM((not v.vote_code)::int), 0) as votes FROM votes v WHERE v.post_id = p.post_id),
         (SELECT COUNT(*) as comment_count FROM comments c WHERE c.post_id = p.post_id)
